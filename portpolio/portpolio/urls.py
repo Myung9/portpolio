baaec59a33ba6bc079ecdcda15ac6f8d03d7bfca
m9 <ymgu0709@gmail.com>
2020-01-12 01:29:34 +0900

    home / detail 수정

--- portpolio/portpolio/urls.py
+++ portpolio/portpolio/urls.py
@@ -20,5 +20,5 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"), # 빈 url이면, views.py의 home을 불러옴
-    path('blog/<init:blog_id>', views.detail, name='detail')
+    path('blog/<int:blog_id>', views.detail, name='detail')
 ]
